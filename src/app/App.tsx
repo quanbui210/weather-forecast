f858be875bd8289177fb8708361fb70af546f165
Quan Bui <quanbui021001@gmail.com>
2026-05-07 18:43:49 +0300

    chore: init project, folder structure setup

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import styles from './App.module.scss'
+
+function App() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.container}>
+        <h1>Weather</h1>
+      </main>
+    </div>
+  )
+}
+
+export default App
